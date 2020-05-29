cac62d395244976298a958c931682f406cd3298c
Aline-G <aline.giordano@live.fr>
2020-05-29 17:32:36 +0200

    Fonction pour modifie la taille en faisant glisser la souris

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -14,6 +14,7 @@ import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Rectangle;
 
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 
 public class Controller {
 
@@ -37,6 +38,8 @@ public class Controller {
     public void initialize() {
 
         this.modele = new Modele(this);
+        ArrayList<Rectangle> listeRec = new ArrayList();
+        ArrayList<Ellipse> listeElli = new ArrayList();
 
         ellipseRB.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
             @Override
@@ -64,33 +67,37 @@ public class Controller {
             @Override
             public void handle(MouseEvent mouseEvent) {
                 couleurClick(mouseEvent);
-                //modele.couleur =  mouseEvent.getValue();
                 System.out.println(modele.getCouleur());
             }
         });
 
         dessin.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
-                    @Override
-                    public void handle(MouseEvent t) {
-                        if (modele.getRec()) {
-
-                        }
-                        System.out.println("dessin mouse pressed");
-                    }
-                });
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (modele.getRec()) {
+                    Rectangle rec = dessinerRec(mouseEvent.getX(), mouseEvent.getY(), modele.getCouleur());
+                    listeRec.add(rec);
+                    dessin.getChildren().add(rec);
+                }
+                if (modele.getElli()) {
+                    Ellipse elli = dessinerElli(mouseEvent.getX(), mouseEvent.getY(), modele.getCouleur());
+                    listeElli.add(elli);
+                    dessin.getChildren().add(elli);
+                }
+            }
+        });
 
 
         dessin.addEventHandler(MouseEvent.MOUSE_DRAGGED, new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
                 if (modele.getRec()) {
-                    System.out.println(modele.getCouleur());
-                    Rectangle rec = dessinerRec(mouseEvent.getX(),mouseEvent.getY(),modele.getCouleur());
-                    dessin.getChildren().add(rec);
+                    resizeRec(listeRec.get(listeRec.size()-1),mouseEvent.getX(), mouseEvent.getY());
+                    dessin.getChildren().add(listeRec.get(listeRec.size()-1));
                 }
-                if (modele.getElli()){
-                    Ellipse elli = dessinerElli(mouseEvent.getX(),mouseEvent.getY(), modele.getCouleur());
-                    dessin.getChildren().add(elli);
+                if (modele.getElli()) {
+                    resizeElli(listeElli.get(listeElli.size()-1),mouseEvent.getX(), mouseEvent.getY());
+                    dessin.getChildren().add(listeElli.get(listeElli.size()-1));
                 }
 
             }
@@ -106,19 +113,7 @@ public class Controller {
                 event.consume();
             }
         });
-
-
-        rectangleRB.isSelected();
-
-        forme.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
-            @Override
-            public void changed(ObservableValue<? extends Toggle> ov, Toggle t, Toggle t1) {
-
-                RadioButton chk = (RadioButton) t1.getToggleGroup().getSelectedToggle(); // Cast object to radio button
-                System.out.println("Selected Radio Button - " + chk.getText());
-
-            }
-        });*/
+*/
     }
 
 
@@ -147,15 +142,18 @@ public class Controller {
         modele.setSelect(true);
     }
     private void couleurClick(MouseEvent event){
-        //ColorPicker cp = new ColorPicker();
-        modele.couleur =  colorP.getValue();
+        modele.setCouleur(colorP.getValue());
     }
 
     private Rectangle dessinerRec(double x, double y, Color couleur){
-        Rectangle rec = new Rectangle (x,y,100,140);
+        Rectangle rec = new Rectangle (x,y,2,2);
         rec.setStroke (Color.BLACK);
         rec.setFill(couleur);
         return rec;
+    }
+    private void resizeRec(Rectangle rec,double x, double y){
+        rec.setHeight(x);
+        rec.setWidth(y);
     }
 
     private Ellipse dessinerElli(double x, double y, Color couleur) {
@@ -163,6 +161,10 @@ public class Controller {
         elli.setStroke (Color.BLACK);
         elli.setFill(couleur);
         return elli;
+    }
+    private void resizeElli(Ellipse elli, double x, double y) {
+        elli.setRadiusX(x);
+        elli.setRadiusX(y);
     }
 
 
