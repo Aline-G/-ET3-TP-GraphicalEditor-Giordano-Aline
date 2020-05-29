73f4ff140be19edb6e93dd83dd2f4ee7b64fce64
Aline-G <aline.giordano@live.fr>
2020-05-29 16:12:37 +0200

    affichage de rectangle et d'une ellipse classe modele faite

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,156 @@
 package sample;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.*;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Rectangle;
+
+import java.awt.event.ActionEvent;
+
 public class Controller {
+
+    private Modele modele;
+    @FXML
+    private RadioButton ellipseRB;
+    @FXML
+    private RadioButton rectangleRB;
+    @FXML
+    private RadioButton lineRB;
+    @FXML
+    private ColorPicker colorP;
+    @FXML
+    private ToggleGroup forme;
+    @FXML
+    private Pane dessin;
+
+
+
+    @FXML
+    public void initialize() {
+
+        this.modele = new Modele(this);
+
+        ellipseRB.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                ellipseClick(mouseEvent);
+                System.out.println(modele.getElli());
+            }
+        });
+        rectangleRB.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                rectangleClick(mouseEvent);
+                System.out.println(modele.getRec());
+            }
+        });
+        lineRB.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                lineClick(mouseEvent);
+                System.out.println(modele.getLine());
+            }
+        });
+/*
+        dessin.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent t) {
+                        if (modele.getRec()) {
+                            dessinerRec(t.getX(),t.getY());
+                        }
+                        System.out.println("dessin mouse pressed");
+                    }
+                });*/
+        dessin.addEventHandler(MouseEvent.MOUSE_DRAGGED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (modele.getRec()) {
+                    System.out.println(modele.getCouleur());
+                    Rectangle rec = dessinerRec(mouseEvent.getX(),mouseEvent.getY(),modele.getCouleur());
+                    dessin.getChildren().add(rec);
+                }
+                if (modele.getElli()){
+                    Ellipse elli = dessinerElli();
+                    dessin.getChildren().add(elli);
+                }
+
+            }
+        });
+/*
+        canvas.setOnDragDetected(new EventHandler() {
+            @Override
+            public void handle(MouseEvent event) {
+                Dragboard db = canvas.startDragAndDrop(TransferMode.ANY);
+                ClipboardContent content = new ClipboardContent();
+                content.putString("Hello!");
+                db.setContent(content);
+                event.consume();
+            }
+        });
+
+
+        rectangleRB.isSelected();
+
+        forme.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override
+            public void changed(ObservableValue<? extends Toggle> ov, Toggle t, Toggle t1) {
+
+                RadioButton chk = (RadioButton) t1.getToggleGroup().getSelectedToggle(); // Cast object to radio button
+                System.out.println("Selected Radio Button - " + chk.getText());
+
+            }
+        });*/
+    }
+
+
+    private void ellipseClick(MouseEvent event){
+        modele.setElli(true);
+        modele.setRec(false);
+        modele.setLine(false);
+        modele.setSelect(false);
+    }
+    private void rectangleClick(MouseEvent event){
+        modele.setElli(false);
+        modele.setRec(true);
+        modele.setLine(false);
+        modele.setSelect(false);
+    }
+    private void lineClick(MouseEvent event){
+        modele.setElli(false);
+        modele.setRec(false);
+        modele.setLine(true);
+        modele.setSelect(false);
+    }
+    private void selectClick(ActionEvent event){
+        modele.setElli(false);
+        modele.setRec(false);
+        modele.setLine(false);
+        modele.setSelect(true);
+    }
+    private void couleurClick(MouseEvent event){
+        //modele.couleur;
+      //  modele.setCouleur();
+    }
+
+    private Rectangle dessinerRec(double x, double y, Color couleur){
+        Rectangle rec = new Rectangle (x,y,100,140);
+        rec.setStroke (Color.BLACK);
+        rec.setFill(couleur);
+        return rec;
+    }
+
+    private Ellipse dessinerElli() {
+        Ellipse elli = new Ellipse (300, 300, 75, 40);
+        elli.setStroke (Color.BLACK);
+        elli.setFill(null);
+        return elli;
+    }
 }
---
+++ src/sample/Modele.java
@@ -0,0 +1,56 @@
+package sample;
+import javafx.scene.paint.Color;
+
+
+public class Modele {
+    private Controller controlleur;
+    public Color couleur;
+    private boolean rec = false;
+    private boolean elli = false;
+    private boolean line = false;
+    private boolean selec = false;
+
+    public Modele(Controller controller){
+        this.controlleur = controller;
+    }
+
+    public boolean getRec(){
+        return rec;
+    }
+    public void setRec(boolean etat){
+        this.rec = etat;
+    }
+    public boolean getElli(){
+        return elli;
+    }
+    public void setElli(boolean etat){
+        this.elli = etat;
+    }
+    public boolean getLine(){
+        return line;
+    }
+    public void setLine(boolean etat){
+        this.line = etat;
+    }
+    public boolean getSelect(){
+        return selec;
+    }
+    public void setSelect(boolean etat){
+        this.selec = etat;
+    }
+    public Color getCouleur(){
+        return couleur;
+    }
+    public void setCouleur(Color couleur){
+        this.couleur= couleur;
+        System.out.println(this.couleur);
+    }
+    public void selectionner(){
+        rec = false;
+        elli = false;
+        line = false;
+        System.out.println("ça marche");
+    }
+
+
+}
